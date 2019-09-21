9611065845b503db779efcd7846c55aab9cca1c3
kivar <ryandam.9@gmail.com>
2019-09-22 09:45:58 +1000

    Created local Maven repo

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ mvn clean install
 cp ./target/DataAnalyzer-1.0-SNAPSHOT.jar ~/Desktop/test_run/  
 cp -r src/main/resources ~/Desktop/test_run/
 cp -r ./target/lib ~/Desktop/test_run/resources/
+cp -r /c/Users/ryand/Desktop/dbutils/src/main/resources/* ~/Desktop/test_run/resources/
 
 cd ~/Desktop/test_run/
-export LOG4J_CONFIGURATION_FILE="C:\Users\ryand\Desktop\test_run\resources\log4j2-da.xml"
+export LOG4J_CONFIGURATION_FILE="C:\Users\ryand\Desktop\test_run\resources\log4j2.xml"
 java --module-path $PATH_TO_FX --add-modules javafx.controls --add-modules javafx.fxml -jar ./DataAnalyzer-1.0-SNAPSHOT.jar
