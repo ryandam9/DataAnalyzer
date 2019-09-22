3f3990f0f8c3be032b11dcac48910f649da13b71
kivar <ryandam.9@gmail.com>
2019-09-22 23:19:04 +1000

    Data Browser

--- src/main/java/com/analyzer/controllers/DataBrowser.java
+++ src/main/java/com/analyzer/controllers/DataBrowser.java
@@ -1,23 +1,21 @@
 package com.analyzer.controllers;
 
-import com.dbutils.common.DBConnections;
+import com.analyzer.classes.RefreshQueryResultsTask;
 import com.dbutils.common.TableDetail;
 import com.dbutils.sqlserver.SqlServerMetadata;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ObservableValue;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
-import javafx.util.Callback;
 
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -46,10 +44,21 @@ public class DataBrowser implements Initializable {
 
     private Connection connection;
     private String dbType;
+    private ProgressIndicator progressIndicator;
+    private Label statusMessage;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Create a Progress Indicator
+        progressIndicator = new ProgressIndicator();
+        progressIndicator.setPrefSize(100, 100);
+        progressIndicator.setVisible(false);
 
+        // Status message
+        statusMessage = new Label();
+        statusMessage.getStyleClass().add("label-level-3");
+
+        statusBar.getChildren().addAll(progressIndicator, statusMessage);
     }
 
     public void setupParameters(String host, Connection connection, String dbType) {
@@ -92,6 +101,9 @@ public class DataBrowser implements Initializable {
 
     @FXML
     private void executeQuery(ActionEvent event) {
+        progressIndicator.setVisible(true);
+        statusMessage.setText("");
+
         String query = queryText.getText();
         ResultSet resultSet;
         ObservableList<ObservableList<String>> queryResultData = FXCollections.observableArrayList();
@@ -100,45 +112,8 @@ public class DataBrowser implements Initializable {
         if (results.getColumns().size() > 0)
             results.getColumns().clear();
 
-        try {
-            resultSet = DBConnections.execReadOnlyQuery(connection, query);
-            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-
-            // Setup Header
-            for (int i = 0; i < resultSet.getMetaData().getColumnCount(); i++) {
-                final int j = i;
-                TableColumn col = new TableColumn(resultSet.getMetaData().getColumnName(i + 1));
-
-                col.setCellValueFactory(
-                        new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
-                            public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
-                                try {
-                                    return new SimpleStringProperty(param.getValue().get(j).toString());
-                                } catch (Exception e) {
-                                    return null;
-                                }
-                            }
-                        });
-
-                results.getColumns().addAll(col);
-                System.out.println("Column [" + i + "] ");
-            }
-
-            // Fetch Data from Result Set
-            while (resultSet.next()) {
-                ObservableList<String> row = FXCollections.observableArrayList();
-                for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
-                    row.add(resultSet.getString(i));
-                }
-                System.out.println("Row [1] added " + row);
-                queryResultData.add(row);
-            }
-
-            // Set Table View with data
-            results.setItems(queryResultData);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Execute the Query in the Background
+        Task<Long> task = new RefreshQueryResultsTask(connection, query, results, progressIndicator, statusMessage);
+        new Thread(task).start();
     }
 }
