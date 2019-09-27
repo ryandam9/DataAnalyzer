e51d6e6027a9b777c56168b68aa66c695881376a
kivar <ryandam.9@gmail.com>
2019-09-28 08:22:12 +1000

    Result Dashboard - tile added

--- src/main/java/com/analyzer/controllers/DashboardController.java
+++ src/main/java/com/analyzer/controllers/DashboardController.java
@@ -1,10 +1,16 @@
 package com.analyzer.controllers;
 
+import com.analyzer.ui.TaskTile;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.TilePane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -29,10 +35,24 @@ public class DashboardController implements Initializable {
     private TextField completedTasks;
 
     @FXML
-    private AnchorPane dashboardArea;
+    private ScrollPane dashboardArea;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Create a Tile pane
+        TilePane tilePane = new TilePane(Orientation.HORIZONTAL);
+        tilePane.setHgap(10.0);
+        tilePane.setVgap(10.0);
+        tilePane.setPadding(new Insets(20, 20, 20, 20));
+        tilePane.setPrefColumns(4);
+        tilePane.setMaxWidth(Region.USE_PREF_SIZE);
 
+        TaskTile tile = new TaskTile("Task12345", 12345L, 123L);
+        tilePane.getChildren().addAll(tile);
+        tile.getStyleClass().add("tile-box");
+
+        dashboardArea.setContent(tilePane);
+        dashboardArea.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        dashboardArea.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
     }
 }
---
+++ src/main/java/com/analyzer/ui/TaskTile.java
@@ -0,0 +1,45 @@
+package com.analyzer.ui;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.VBox;
+
+public class TaskTile extends VBox {
+    private Label taskName;
+    private Label totalRecordsToScan;
+    private Label processedSoFar;
+    private ProgressIndicator progressIndicator;
+
+    public TaskTile(String taskName, Long totalRecordsToScan, Long processedSoFar) {
+        this.getStyleClass().add("tile-box");
+
+        this.taskName = createLabel(taskName);
+        this.totalRecordsToScan = createLabel(totalRecordsToScan.toString());
+        this.processedSoFar = createLabel(processedSoFar.toString());
+
+        // Progress Indicator
+        progressIndicator = new ProgressIndicator();
+        progressIndicator.getStyleClass().add("progress-indicator");
+        progressIndicator.setVisible(true);
+
+        getChildren().addAll(this.taskName, this.totalRecordsToScan, this.processedSoFar, this.progressIndicator);
+    }
+
+    private Label createLabel(String text) {
+        Label label = new Label(text);
+        label.getStyleClass().add("tile-label");
+        return label;
+    }
+
+    public ProgressIndicator getProgressIndicator() {
+        return progressIndicator;
+    }
+
+    public void setProcessedSoFar(Long processedSoFar) {
+        this.processedSoFar = createLabel(processedSoFar.toString());
+    }
+
+    public void setProgressIndicator(boolean visibility) {
+        this.progressIndicator.setVisible(visibility);
+    }
+}
